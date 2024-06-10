de41d91880f08cfeba799d473de99a1b355b26a3
JJQKR <yjcoyjco@naver.com>
2024-06-10 15:35:41 +0900

    5th TimeAttack Calculator

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,1 +1,10 @@
 // TODO: store 셋업하고 slice에서 import한 reducer를 할당하세요.
+
+import calculatorSlice from "../slices/calculatorSlice";
+import { configureStore } from "@reduxjs/toolkit";
+
+export default configureStore({
+  reducer: {
+    calculator: calculatorReducer,
+  },
+});
--- src/redux/slices/calculatorSlice.js
+++ src/redux/slices/calculatorSlice.js
@@ -1,1 +1,22 @@
 // TODO: result 상태와 덧셈, 뺄셈에 대한 상태변경 로직을 담은 slice를 만들어 보세요.
+
+import { createSlice } from "@reduxjs/toolkit";
+
+export const calculatorSlice = createSlice({
+  name: "calculatorSlice",
+  initialState: {
+    number: 0,
+  },
+  reducers: {
+    plus: (state, payload) => {
+      state.number += action.payload;
+    },
+
+    minus: (state, payload) => {
+      state.number -= action.payload;
+    },
+  },
+});
+
+export const { plus, minus } = calculatorSlice.actions;
+export default calculatorSlice.reducer;
